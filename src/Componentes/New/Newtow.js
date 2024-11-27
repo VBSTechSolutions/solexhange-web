2cf129e89b78c9997dc3a6d358723b16d5887a7d
himanshu <himanshupokhriyal655@gmail.com>
2024-11-27 18:06:22 +0530

    last commite end

--- src/Componentes/New/Newtow.js
+++ src/Componentes/New/Newtow.js
@@ -11,7 +11,7 @@ function Newtow() {
             <img src={images.news2} className="img-fluid new2-imge " alt="Card example" />
           </div>
           <div className="col-md-6 p-5 bg-dark text-white">
-            <div className="ps-5">
+            <div className="ps-5 mt-5 ms-5 magrinen">
               <h1 className='newsheading'>
               Best Men's
               </h1>
@@ -25,12 +25,18 @@ function Newtow() {
                <h1 className='newsheading'>
                Deals on 
               </h1>
-              <p className="">
+              <p className="newspargarpha mt-5">
                 This is a wider card with supporting text below as a natural lead-in to additional content. 
                 This content is a little bit longer.
               </p>
               
             </div>
+            <button type="button " className="btn btn-dark btn-lg mt-5 news_btn d-flex w-50 right_zero">
+                <p className="m-0">Read more
+                  </p>       <p className="m-0">
+                        ++++++
+                      </p>
+                    </button>
           </div>
         </div>
       </div>
